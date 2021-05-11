dcd6c0421d500d75c708c48f4c0a90a91e3b693b
Julio Lima <juliow300@gmail.com>
2021-05-11 20:49:20 -0300

    Veiculos

---
+++ src/br/com/myauto/dominio/modelo/veiculo/Veiculo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.myauto.dominio.modelo.veiculo;
+
+/**
+ *
+ * @author julio
+ */
+public class Veiculo {
+    
+    public long id;
+//    public enum TipoDeCombustivel;
+    public String tipo;
+    public String marca;
+    public String modelo;
+    public int ano;
+    public String apelido;
+    public String volumeDoTanque;
+    public boolean doisTanques;
+    public String placa;
+    public String chassi;
+    public String renavan;
+    public String comentario;
+    
+}
---
+++ src/br/com/myauto/dominio/veiculo/RepositorioDeVeiculo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.myauto.dominio.veiculo;
+
+import br.com.myauto.dominio.modelo.usuario.Usuario;
+import br.com.myauto.dominio.modelo.veiculo.Veiculo;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author julio
+ */
+public interface RepositorioDeVeiculo {
+    
+    List<Veiculo> listarVeiculo() throws SQLException;
+	
+    void inserirVeiculo(Veiculo veiculo) throws SQLException;
+
+    boolean editarVeiculo(Veiculo veiculo) throws SQLException;
+
+    boolean excluirVeiculo(int id) throws SQLException;
+
+    Usuario buscarVeiculoPorId(int id) throws SQLException;
+    
+}
